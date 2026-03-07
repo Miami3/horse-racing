44c7771487e21f024aff53c85a87d9d66f3088a1
Oleg Kalysh <olegkalysh25@gmail.com>
2026-03-08 00:18:20 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <header>
+    Header
+  </header>
+  <main>
+    main
+    <Button>Button</Button>
+  </main>
+  <footer>
+    footer
+  </footer>
+</template>
+
+<style scoped></style>
